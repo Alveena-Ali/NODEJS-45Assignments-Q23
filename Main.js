77c0887900fb03d42347993e79600e081db0d17d
Alveena-Ali <163353918+Alveena-Ali@users.noreply.github.com>
2024-09-08 12:45:15 +0500

    Que 23: Conditional Tests

---
+++ Main.js
@@ -0,0 +1,41 @@
+"use strict";
+// Que 23: Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your 
+// prediction for the results of each test. Your code should look something like this:
+Object.defineProperty(exports, "__esModule", { value: true });
+// let car = 'subaru';
+// console.log("Is car == 'subaru'? I predict True.")
+// console.log(car == 'subaru')
+// • Look closely at your results, and make sure you understand why each line evaluates to True or False.
+// • Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+// Creating Car Variable
+let car = 'sabaru';
+// Test No 1
+console.log("Is car === 'subaru'? I predict True.");
+console.log(car === 'sabaru');
+// Test No 2
+console.log("Is car == 'subaru'? I predict True.");
+console.log(car == 'sabaru');
+// Test No 3
+console.log("Is car != 'honda'? I predict True.");
+console.log(car != 'honda');
+// Test No 4
+console.log("Is car !== 'ford'? I predict True.");
+console.log(car !== 'ford');
+// Test No 5
+console.log("Is car.Uppercase == 'SABARU'? I predict True.");
+console.log(car.toUpperCase() == 'SABARU');
+// Test No 6
+console.log("Is car == 'SABARU'? I predict False.");
+console.log(car == 'SABARU');
+// Test No 7
+console.log("Is car === 'SABARU'? I predict False.");
+console.log(car === 'SABARU');
+// Test No 8
+console.log("Is car === 'Train'? I predict False.");
+console.log(car === 'Train');
+// Test No 9
+console.log("Is car === 'Bus'? I predict False.");
+console.log(car === 'Bus');
+// Test No 10
+console.log("Is car === 'Cycle'? I predict False.");
+console.log(car === 'Cycle');
